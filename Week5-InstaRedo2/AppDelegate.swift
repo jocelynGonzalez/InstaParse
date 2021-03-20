240e22b43ecfb4a983e36f6e13003339b1209102
jocelynGonzalez <gonzalezjocelyn2005@gmail.com>
2021-03-19 23:10:19 -0700

    First part of parstagram

--- Week5-InstaRedo2/AppDelegate.swift
+++ Week5-InstaRedo2/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Parse
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "X0LXXzg2KpDGFnGjvviLg6Sk7YVD6gHyunbfa3xf" // <- UPDATE
+                $0.clientKey = "ctE1dLjDqPLJi9iJUGqf5Edx1VBb7Uyn5g6h0u4B" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
